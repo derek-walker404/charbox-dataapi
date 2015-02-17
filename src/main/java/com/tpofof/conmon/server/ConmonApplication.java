500fe7386903cbf86589d247fdae83e153ef358f
david <david@tpofof.com>
2015-02-17 13:32:20 -0500

    Adding device CR functionality. Converting to IntelliJ project.

--- src/main/java/com/tpofof/conmon/server/ConmonApplication.java
+++ src/main/java/com/tpofof/conmon/server/ConmonApplication.java
@@ -5,11 +5,14 @@ import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 import com.tpofof.conmon.server.config.ConmonConfiguration;
 import com.tpofof.conmon.server.config.MongoConfig;
+import com.tpofof.conmon.server.managers.DeviceConfigurationManager;
+import com.tpofof.conmon.server.managers.DeviceManager;
 import com.tpofof.conmon.server.mongo.DeviceConfigDAO;
+import com.tpofof.conmon.server.mongo.DeviceDAO;
 import com.tpofof.conmon.server.mongo.TestCaseDAO;
+import com.tpofof.conmon.server.resources.DeviceResource;
 import com.tpofof.conmon.server.resources.crud.DeviceConfigResource;
 import com.tpofof.conmon.server.resources.crud.TestCaseResource;
-
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
@@ -32,19 +35,27 @@ public class ConmonApplication extends Application<ConmonConfiguration> {
 	@Override
 	public void run(ConmonConfiguration config, Environment env) throws Exception {
 		/* MONGO */
-		MongoConfig mdbConfig = new MongoConfig(); //config.getMongo();
-		MongoClient mongoClient = new MongoClient(mdbConfig.getHost(), mdbConfig.getPort());
-		DB conmonDb = mongoClient.getDB(mdbConfig.getDbname());
-		DBCollection deviceConfigCollection = conmonDb.getCollection("deviceConfig");
-		DeviceConfigDAO deviceConfigDAO = new DeviceConfigDAO(deviceConfigCollection);
-		DBCollection testCaseCollection = conmonDb.getCollection("testCase");
-		TestCaseDAO testCaseDAO = new TestCaseDAO(testCaseCollection);
+		final MongoConfig mdbConfig = new MongoConfig(); //config.getMongo();
+		final MongoClient mongoClient = new MongoClient(mdbConfig.getHost(), mdbConfig.getPort());
+		final DB conmonDb = mongoClient.getDB(mdbConfig.getDbname());
+		final DBCollection deviceConfigCollection = conmonDb.getCollection("deviceConfig");
+		final DeviceConfigDAO deviceConfigDAO = new DeviceConfigDAO(deviceConfigCollection);
+		final DBCollection testCaseCollection = conmonDb.getCollection("testCase");
+		final TestCaseDAO testCaseDAO = new TestCaseDAO(testCaseCollection);
+		final DBCollection deviceCollection = conmonDb.getCollection("device");
+		final DeviceDAO deviceDao = new DeviceDAO(deviceCollection);
+		
+		/* MANAGERS */
+		final DeviceConfigurationManager deviceConfigMan = new DeviceConfigurationManager(deviceConfigDAO, testCaseDAO);
+		final DeviceManager deviceMan = new DeviceManager(deviceDao, deviceConfigMan);
 		
 		/* RESOURCES */
 		final DeviceConfigResource deviceConfigResource = new DeviceConfigResource(deviceConfigDAO);
 		env.jersey().register(deviceConfigResource);
 		final TestCaseResource testCaseResource = new TestCaseResource(testCaseDAO);
 		env.jersey().register(testCaseResource);
+		final DeviceResource deviceResource = new DeviceResource(deviceMan);
+		env.jersey().register(deviceResource);
 	}
 
 }
--- src/main/java/com/tpofof/conmon/server/resources/DeviceResource.java
+++ src/main/java/com/tpofof/conmon/server/resources/DeviceResource.java
@@ -1,27 +1,59 @@
 package com.tpofof.conmon.server.resources;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
-
 import com.codahale.metrics.annotation.Timed;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.base.Optional;
+import com.pofof.conmon.model.Device;
+import com.tpofof.conmon.server.managers.DeviceManager;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/devices")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class DeviceResource {
 
+	private final DeviceManager deviceMan;
+	
+	public DeviceResource(DeviceManager deviceMan) {
+		this.deviceMan = deviceMan;
+	}
+	
 	@GET
 	@Timed
 	public JsonNode find(@QueryParam("limit") Optional<Integer> limit,
 			@QueryParam("offset") Optional<Integer> offset) {
-		JsonNode node = null;
-		
-		return node;
+		int limitVal = limit.isPresent() && limit.get() > 0 ? limit.get() : 10;
+		int offsetVal = offset.isPresent() && offset.get() >= 0 ? offset.get() : 0;
+		List<Device> devices = deviceMan.getDevices(limitVal, offsetVal);
+		return ResponseUtils.success(ResponseUtils.listData(devices, limitVal, offsetVal));
+	}
+	
+	@Path("/{_id}")
+	@GET
+	@Timed
+	public JsonNode find(@PathParam("_id") String id) {
+		Device device = deviceMan.getDevice(id);
+		return device != null
+				? ResponseUtils.success(ResponseUtils.modelData(device))
+				: ResponseUtils.failure("Could not find Device with id " + id, 404);
+	}
+	
+	@Path("/id/{deviceId}")
+	@GET
+	@Timed
+	public JsonNode findByDeviceId(@PathParam("deviceId") Integer deviceId) {
+		Device device = deviceMan.findByDeviceId(deviceId);
+		return ResponseUtils.success(ResponseUtils.modelData(device));
+	}
+	
+	@Path("/register/{deviceId}")
+	@POST
+	@Timed
+	public JsonNode register(@PathParam("deviceId") Integer deviceId) {
+		Device device = deviceMan.register(deviceId);
+		return ResponseUtils.success(ResponseUtils.modelData(device));
 	}
 }
