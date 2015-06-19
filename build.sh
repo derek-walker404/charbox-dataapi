ecaa4d0f709dfb5512e3534c55dff0b0bdcc049f
David Esposito <desposi1@gmail.com>
2015-06-19 18:55:28 -0400

    Updating deployment scripts

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-cd ../tpofof-core/;mvn clean install; cd ../charbox-dataapi/
-cd ../tpofof-dwa/;mvn clean install; cd ../charbox-dataapi/
-cd ../charbox-domain/;mvn clean install; cd ../charbox-dataapi/
-cd ../charbox-core/;mvn clean install; cd ../charbox-dataapi/
-cd ../charbox-dataapi/;mvn clean install; cd ../charbox-dataapi/
--- push_zip2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-scp -i ~/.ssh/charbot-stage.pem temp/charbot-dataapi.zip ubuntu@ec2-54-84-219-171.compute-1.amazonaws.com:~
